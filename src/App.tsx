09fcbf0601ce221268673f479a0f4eff21f56431
Atif Zaman <atifzaman88010@gmail.com>
2025-04-13 23:38:19 +0200

    Initial project setup and necessary packages installation

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from 'semantic-ui-react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header as="h1">Welcome to Comeon frontend challagne</Header>
+    </>
+  )
+}
+
+export default App
